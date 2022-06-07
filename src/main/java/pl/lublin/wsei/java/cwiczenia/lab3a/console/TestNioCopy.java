3f7c458e619bc6155ca0b507d46e48b7bdc6db16
PZrobek <37156@studentwsei.pl>
2022-06-07 22:31:12 +0200

    Lab 3a - •	Klasa TestNioCopy - pomiar czasu kopiowania za pomocą metody currentTimeMillis();

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab3a/console/TestNioCopy.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3a/console/TestNioCopy.java
@@ -8,12 +8,12 @@ import java.nio.file.StandardCopyOption;
 
 public class TestNioCopy {
     public static void main(String[] args) throws IOException {
-//        long startTime = System.currentTimeMillis();
-        long startTime = System.nanoTime();
+        long startTime = System.currentTimeMillis();
+//        long startTime = System.nanoTime();
         Path oryginal = Paths.get("sample_1920x1280.tiff");
         Path kopia = Paths.get("img_copy2.tiff");
         Files.copy(oryginal, kopia, StandardCopyOption.REPLACE_EXISTING);
-//        System.out.println("Czas kopiowania: " + (System.currentTimeMillis() - startTime)+" ms");
-        System.out.println("Czas kopiowania: " + (float)(System.nanoTime() - startTime)/1000000+" ms");
+        System.out.println("Czas kopiowania: " + (System.currentTimeMillis() - startTime)+" ms");
+//        System.out.println("Czas kopiowania: " + (float)(System.nanoTime() - startTime)/1000000+" ms");
     }
 }

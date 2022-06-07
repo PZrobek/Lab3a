5cb8930069216994032b1f594d029da3898d09e3
PZrobek <37156@studentwsei.pl>
2022-06-07 23:03:10 +0200

    Lab 3a - •	Alternatywna metoda kopiowania i zapisu plików

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab3a/console/TestNioCopy.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3a/console/TestNioCopy.java
@@ -1,5 +1,6 @@
 package pl.lublin.wsei.java.cwiczenia.lab3a.console;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,12 +9,12 @@ import java.nio.file.StandardCopyOption;
 
 public class TestNioCopy {
     public static void main(String[] args) throws IOException {
-        long startTime = System.currentTimeMillis();
-//        long startTime = System.nanoTime();
-        Path oryginal = Paths.get("sample_1920x1280.tiff");
-        Path kopia = Paths.get("img_copy2.tiff");
-        Files.copy(oryginal, kopia, StandardCopyOption.REPLACE_EXISTING);
-        System.out.println("Czas kopiowania: " + (System.currentTimeMillis() - startTime)+" ms");
-//        System.out.println("Czas kopiowania: " + (float)(System.nanoTime() - startTime)/1000000+" ms");
+//        long startTime = System.currentTimeMillis();
+        long startTime = System.nanoTime();
+        File oryginal = new File("sample_1920x1280.tiff");
+        File kopia = new File("img_copy3.tiff");
+        Files.copy(oryginal.toPath(), kopia.toPath());
+//        System.out.println("Czas kopiowania: " + (System.currentTimeMillis() - startTime)+" ms");
+        System.out.println("Czas kopiowania: " + (float)(System.nanoTime() - startTime)/1000000+" ms");
     }
 }
